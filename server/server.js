ec3c620677e610d52cd8d0ec722e33233d73e071
glennh2023 <glenn.hadcock@gmail.com>
2024-11-09 12:11:41 -0600

    FIX SERVER

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ app.get("/api/message", (req, res) => {
 });
 
 // Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Catch-all handler for any request that doesn't match an API route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 // Start server

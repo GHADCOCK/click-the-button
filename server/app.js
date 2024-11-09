a4445fbfc69f72cd9f1c366a014734caca5ca147
glennh2023 <glenn.hadcock@gmail.com>
2024-11-09 11:03:16 -0600

    Added a catch all and made it serve static requests

--- server/app.js
+++ server/app.js
@@ -14,6 +14,14 @@ app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from the server! F YA" });
 });
 
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// Catch-all handler for any request that doesn't match an API route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
